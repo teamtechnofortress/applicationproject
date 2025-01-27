3860ac8143c30b605f97279f2888c760d112a90a
Zulnurain <meet.zulnurain@gmail.com>
2025-01-27 13:59:54 +0500

    delete

--- pages/Mosque-Prayer-Timetable/mptt-help.php
+++
@@ -1,9 +0,0 @@
-<br><div class=""><h1>MPTT Help</h1></div><br><br>
-
-<ul>
-    <li><h3>For home widget use <code>[home-widget]</code> shortcode any where in the page, blog post.</h3></li>
-    <li><h3>For Daily timer or mobile widget use <code>[mobile-widget]</code> shortcode any where in the page, blog post.</h3></li>
-    <li><h3>For full time table use <code>[full-timetable]</code> shortcode to display full timetable. </h3></li>
-    <li><h3>To setup the colors for the text and font size, borders, widgets background, and other options check the settings.</h3></li>
-	<li><h3>You can download sample csv file from <a download="sample" href="<?php echo plugin_dir_url( __FILE__).'sample.csv'?>">here</a>.</h3></li>
-</ul>
